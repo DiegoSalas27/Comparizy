f8d46d47867dba86a4fef6880c842c481150afb0
Diego Salas <dominicsc2hs@gmail.com>
2021-06-26 17:11:09 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+from tecnologia import *
+
+if __name__ == '__main__':
+    while True:
+        find_products_tecnologia_computadoras()
+        time_wait = 1440
+        print(f'Waiting {time_wait} minutes...')
+        time.sleep(time_wait * 60) #run program every day\
