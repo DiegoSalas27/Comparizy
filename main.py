16c24ed61223e5d41247ea17854694dc49862fc5
Diego Salas <dominicsc2hs@gmail.com>
2021-07-22 23:55:33 -0500

    done

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from tecnologia import *
 if __name__ == '__main__':
     while True:
         # find_products_tecnologia_televisores()
-        find_products_tecnologia_computadoras()
+        find_products_tecnologia_computadoras_saga()
+        find_products_tecnologia_computadoras_ripley()
+        find_products_tecnologia_computadoras_oeschle()
         time_wait = 1440
         print(f'Waiting {time_wait} minutes...')
         time.sleep(time_wait * 60) #run program every day\
